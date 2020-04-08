3e146bf8a2150bafd4f005d3530e701592d3fcce
unknown <reinier.van.limpt@gmail.com>
2020-04-08 16:37:09 +0200

    added stylized message commands

--- server.js
+++ server.js
@@ -19,13 +19,34 @@ function open(req, res) {
   res.render('index.ejs')
 }
 
-io.on('connection', function(socket) { // bij een nieuwe connectie, voer dit uit:
-  console.log('a user connected');
+io.on('connection', function(socket) { // de code die uitegvoerd wordt voor elke connectie/socket:
+
+  socket.username = `anonymous`
+
+  socket.broadcast.emit(`${socket.username} joined the chat`)
+
+  socket.on('new user', function(id) {
+    oldName = socket.username
+    socket.username = id
+    socket.broadcast.emit('server message', `${oldName} changed their name to ${socket.username}!`)
+    socket.emit('server message', `welcome to the chat ${socket.username}!`)
+  });
+
   socket.on('disconnect', function() { // bij een connectie die verbroken wordt, voer dit uit:
     console.log('user disconnected');
-    console.log(socket.server.sockets.sockets) // door het socket object heen gespit op zoek naar nog actieve sockets
   });
+
   socket.on('chat message', function(msg) {
-    io.emit('chat message', msg);
+    const stringArray = msg.split(" ");
+    const textStyle = stringArray[0]
+    if (textStyle.startsWith('/')) {
+      io.emit('chat message', `${socket.username} : ${msg.substring(textStyle.length)}`, `${textStyle.substring(1)}`);
+    } else {
+      io.emit('chat message', `${socket.username} : ${msg}`);
+    }
+  });
+
+  socket.on('server message', function(msg) {
+    io.emit('server message', msg);
   });
 });
