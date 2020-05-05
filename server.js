a1db6a14ebd809b45949da282ca7ceb2b1792b37
unknown <reinier.van.limpt@gmail.com>
2020-05-05 20:29:13 +0200

    realtime data on characters in game and events are shown

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = express()
 const fs = require('fs')
 const request = require('request')
 
-const eventLog = []
+
+let eventLog = []
 const userBets = []
+let teamChaos = []
+let teamOrder = []
+
 const userPoints = 0
 
 const options = {
@@ -31,10 +35,40 @@ function open(req, res) {
   res.render('index.ejs')
 }
 
-io.on('connection', function(socket) { // de code die uitegvoerd wordt voor elke connectie/socket:
+io.on('connection', function(socket) { // de code die uitgevoerd wordt voor elke connectie/socket:
+
+  eventLog = []
+
+  function createTeams() {
+
+    request({
+      method: 'GET',
+      uri: 'https://127.0.0.1:2999/liveclientdata/allgamedata',
+      agentOptions: {
+        ca: options.ca
+      }
+    }, (err, res, body) => {
+      const data = JSON.parse(body)
+      const allPlayers = data.allPlayers
+      if (teamOrder.length === 0) {
+        allPlayers.forEach(element => assignTeam(element))
+      }
+
+      function assignTeam(player) {
+        if (player.team === "CHAOS") {
+          teamChaos.push(player)
+        } else if (player.team === "ORDER") {
+          teamOrder.push(player)
+        }
+      }
+      teamChaos.forEach(element => socket.emit('team chaos', `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${element.championName}_0.jpg`, `${element.championName}`, "0 / 0 / 0"))
+      teamOrder.forEach(element => socket.emit('team order', `http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${element.championName}_0.jpg`, `${element.championName}`, "0 / 0 / 0"))
+    })
+  }
+
+  createTeams()
 
   function checkForEvents() {
-    console.log("check")
     request({
       method: 'GET',
       uri: 'https://127.0.0.1:2999/liveclientdata/allgamedata',
@@ -45,22 +79,39 @@ io.on('connection', function(socket) { // de code die uitegvoerd wordt voor elke
       const data = JSON.parse(body)
       const allEventsLength = data.events.Events.length
       const allEvents = data.events.Events
+      const allPlayers = data.allPlayers
 
       if (eventLog.length < allEventsLength && eventLog.length === 0) {
-
-        eventLog.forEach(element => socket.emit('new event', `${element.EventName}`, `${element.EventTime}`))
         allEvents.forEach(element => eventLog.push(element))
+        eventLog.forEach(element => checkEventType(element))
       } else if (eventLog.length < allEventsLength) {
         const latestEvent = allEventsLength - 1
-        socket.emit('new event', `${allEvents[latestEvent].EventName}`)
         eventLog.push(allEvents[latestEvent])
+        checkEventType(eventLog[eventLog.length - 1])
       }
+      allPlayers.forEach(element => checkPulse(element))
     })
     console.log(eventLog)
     setTimeout(checkForEvents, 1000)
   }
 
   checkForEvents()
+
+  function checkEventType(gameEvent) {
+    if (gameEvent.EventName === "ChampionKill") {
+      socket.emit('champion kill event', `${gameEvent.EventName}`, `${gameEvent.KillerName}`, `${gameEvent.VictimName}`)
+    } else {
+      socket.emit('new event', `${gameEvent.EventName}`)
+    }
+  }
+
+  function checkPulse(champion) {
+    if (champion.isDead === false) {
+      socket.emit('champion status', "alive", `${champion.championName}`)
+    } else {
+      socket.emit('champion status', "dead", `${champion.championName}`)
+    }
+  }
 
   socket.username = `anonymous`
 
--- static/client.js
+++ static/client.js
@@ -3,19 +3,19 @@ const chatForm = document.querySelector('.message')
 const userForm = document.querySelector(".username")
 const messages = document.querySelector("#messages")
 const events = document.querySelector("#events")
+const kills = document.querySelector("#championKills")
+const teamChaos = document.querySelector("#teamChaos")
+const teamOrder = document.querySelector("#teamOrder")
 const nameSelector = document.querySelector("#nameSelector")
 
 const socket = io(); // maakt een globale variabele van io dit is een connectie met de http server
 
 userForm.addEventListener("submit", function(e) {
-  console.log(document.querySelector('#u').value)
   e.preventDefault() // prevents page reloading
   socket.emit('new user', document.querySelector('#u').value);
   nameSelector.classList.add("hidden")
   return false
-});
-
-
+})
 
 chatForm.addEventListener("submit", function(e) {
   e.preventDefault(); // prevents page reloading
@@ -23,7 +23,6 @@ chatForm.addEventListener("submit", function(e) {
   const stringArray = message.split(" ")
   const messageStyle = stringArray[0]
   const assignedStyle = commands.includes(messageStyle)
-  console.log(message)
 
   if (assignedStyle == true) {
     socket.emit('styled message', message.substring(messageStyle.length), messageStyle.substring(1));
@@ -39,36 +38,82 @@ chatForm.addEventListener("submit", function(e) {
     m.value = ""
     return false
   }
-});
+})
 
-socket.on('new event', function(msg, time) {
+socket.on('team chaos', function(championSplash, championName, championScore) {
+  const newChampionCard = document.createElement("article")
+  const newChampion = document.createElement("img")
+  const newChampionName = document.createElement("p")
+  const newChampionScore = document.createElement("p")
+  newChampion.classList.add(championName)
+  newChampionName.textContent = championName
+  newChampion.src = championSplash
+  teamChaos.appendChild(newChampionCard)
+  newChampionCard.appendChild(newChampion)
+  newChampionCard.appendChild(newChampionName)
+})
+
+socket.on('team order', function(championSplash, championName, championScore) {
+  const newChampionCard = document.createElement("article")
+  const newChampion = document.createElement("img")
+  const newChampionName = document.createElement("p")
+  const newChampionScore = document.createElement("p")
+  newChampion.classList.add(championName)
+  newChampionName.textContent = championName
+  newChampion.src = championSplash
+  teamOrder.appendChild(newChampionCard)
+  newChampionCard.appendChild(newChampion)
+  newChampionCard.appendChild(newChampionName)
+})
+
+socket.on('new event', function(msg) {
   const newMessage = document.createElement("li")
-  newMessage.textContent = msg + " time " + time
+  newMessage.textContent = msg
   events.appendChild(newMessage)
-});
+})
+
+socket.on('champion kill event', function(eventName, killer, victim) {
+  const newKillCard = document.createElement("article")
+  const newKillMessage = document.createElement("p")
+  const killOrder = document.createElement("p")
+  newKillMessage.textContent = eventName
+  killOrder.textContent = killer + " > " + victim
+  kills.appendChild(newKillCard)
+  newKillCard.appendChild(newKillMessage)
+  newKillCard.appendChild(killOrder)
+})
+
+socket.on('champion status', function(status, championName) {
+  const champion = document.getElementsByClassName(`${championName}`)[0]
+  if (status === "dead") {
+    champion.classList.add("dead")
+  } else {
+    champion.classList.remove("dead")
+  }
+})
 
 socket.on('styled message', function(msg, style) {
   const newMessage = document.createElement("li")
   newMessage.textContent = msg
   newMessage.classList.add(style)
   messages.appendChild(newMessage)
-});
+})
 
 socket.on('chat message', function(msg) {
   const newMessage = document.createElement("li")
   newMessage.textContent = msg
   messages.appendChild(newMessage)
-});
+})
 
 socket.on('server message', function(msg) {
   const newMessage = document.createElement("li")
   newMessage.textContent = msg
   messages.appendChild(newMessage)
-});
+})
 
 socket.on('error message', function(errmessage) {
   const newMessage = document.createElement("li")
   newMessage.textContent = errmessage
   newMessage.classList.add("error")
   messages.appendChild(newMessage)
-});
+})
